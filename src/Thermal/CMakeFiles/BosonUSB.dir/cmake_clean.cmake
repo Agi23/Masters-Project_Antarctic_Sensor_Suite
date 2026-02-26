a4c4b980e253fd01e354b0e98e0416ca060cf610
Daniel Jones <danmax.jones@gmail.com>
2026-02-26 12:44:06 +0200

    Added thermal base code

---
+++ src/Thermal/CMakeFiles/BosonUSB.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "BosonUSB"
+  "BosonUSB.pdb"
+  "CMakeFiles/BosonUSB.dir/BosonUSB.cpp.o"
+  "CMakeFiles/BosonUSB.dir/BosonUSB.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BosonUSB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
